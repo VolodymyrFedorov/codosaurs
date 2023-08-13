c90197ef5b7b0ce04b1b7ed48a7bc09c75701572
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-08-13 16:48:29 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,12 @@ import './hero';
 import './upcoming-this-mounth';
 import './weekly-trends';
 import './modal';
+
+import './api';
+import './container';
+import './header-modal';
+import './heroUi';
+import './loader';
+import './swiper';
+import './tmbd-api';
+import './trailer-modal';
